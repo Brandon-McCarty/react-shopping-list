7da76e62b6fe807240eb327e6ac7c9b3359a3ce5
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-03-24 14:35:06 -0500

    adds basic button and list item structure

---
+++ src/components/Items/Items.jsx
@@ -0,0 +1,20 @@
+function Items({item, deleteItem}) {
+    console.log('I am self aware, you are in Items.jsx: ', item);
+
+    const handlePurchase = () => {
+        console.log('BUY BUY BUY')
+    }
+
+
+    return (
+        <>
+        <li>Item Name: {item.name} | Amount: {item.quantity} {item.unit} 
+        <span><button onClick={(event) => {handlePurchase}}>Buy</button></span>
+        <span><button onClick={(event) => {deleteItem(item)}}>Delete</button></span>
+        </li>
+            <br />
+        </>
+    )
+}
+
+export default Items;
